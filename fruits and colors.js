1203b3a87d6d93d5ba04eadde4faa913b05ba090
Farah <obaidatfarah4@gmail.com>
2021-04-20 11:34:44 +0300

    first commit

---
+++ fruits and colors.js
@@ -0,0 +1,19 @@
+let color=prompt('enter color',);
+switch(color){
+    case("red"):
+    alert("apple");
+    break;
+    case("yellow"):
+    alert("banana");
+    break;
+    case("green"):
+    alert("grass");
+    break;
+    case("sequre"):
+    case("retangle"):
+    case("circle"):
+    alert("shape");
+    break;
+    default:
+        alert("i dont know");
+}
